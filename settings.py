2ef7e1897ea851350143a4bf456c5c79e99cf404
René Ribaud <rene.ribaud@free.fr>
2012-11-06 20:46:45 +0100

    Add django app : django-extension + Werkzeug (dep), south.

--- settings.py
+++ settings.py
@@ -144,6 +144,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'django_extensions',
+    'south',
     'debug_toolbar',
     'ot_logbook',
 )
