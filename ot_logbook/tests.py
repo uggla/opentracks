23608ce7d6c1d59e5890a2f7c58368ec321117c0
René Ribaud <rene.ribaud@free.fr>
2012-11-06 20:22:53 +0100

    Make a unit test example. Add initial_data.json as a first set of fixture. May change later...

--- ot_logbook/tests.py
+++ ot_logbook/tests.py
@@ -6,7 +6,8 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from ot_logbook.models import Activity
+from datetime import datetime, date, time, tzinfo, timedelta
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +15,18 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+    def test_basic_multiplication(self):
+        """
+        Tests that 2 * 2 always equals 4.
+        """
+        self.assertEqual(2 * 2, 4)
+
+class ActivityTest(TestCase):
+    def test_create_activity(self):
+        """
+        Get activity id=418
+        Make sure start point is {'lat': 45.2759208679199, 'lon': 5.6514458656311}
+        """
+        act=Activity.get_id(418)
+        self.assertEqual(act.start(),{'lat': 45.2759208679199, 'lon': 5.6514458656311})
